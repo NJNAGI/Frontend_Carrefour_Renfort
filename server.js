dfb9f257b9da32b698d938d5b7ff1338ddde2698
NJNAGI <58658508+NJNAGI@users.noreply.github.com>
2021-12-01 15:53:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/renfort'));
+app.use(express.static('./dist/renfort1'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/renfort/'}),
+    res.sendFile('index.html', {root: 'dist/renfort1/'}),
 );
 
 // Start the app by listening on the default Heroku port
